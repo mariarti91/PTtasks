ada8c5943f11380214a243f45b97e64bcf2830c8
Your Name <you@example.com>
2018-05-05 17:29:28 +0700

    some renames

--- test.sh
+++ test.sh
@@ -5,17 +5,15 @@ docker build -t forpytest  ./img-ubuntu-sshd/ &> /dev/null
 echo -e "\t\tdone"
 
 echo -en "running docker container for test..."
-docker run -p 22022:22 --name pytestcontainer -d forpytest &> /dev/null
+docker run -p 22022:22 --name pytestcontainer --rm -d forpytest &> /dev/null
 echo -e "\tdone"
 
+pushd src
 py.test -s -v test.py
+popd
 
 echo -en "stopping docker container..."
 docker stop pytestcontainer &> /dev/null
-echo -e "\t\tdone"
-
-echo -en "removing docker container..."
-docker rm pytestcontainer &> /dev/null
 echo -e "\t\tdone"
 
 echo -en "removing docker image..."
