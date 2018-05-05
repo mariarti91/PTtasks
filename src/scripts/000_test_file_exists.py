05a671c15e9900034cd56e15262857323c737684
Your Name <you@example.com>
2018-05-05 21:34:45 +0700

    file existing checker

--- src/scripts/000_test_file_exists.py
+++ src/scripts/000_test_file_exists.py
@@ -1,8 +1,17 @@
-print("Hi I'm test module")
+from transports import get_transport
+from transports import TransportConnetionError
 
-def mistake(data=[]):
-    print(data)
-    return data
-    
-def mistanke_explained(f=print("I'm called")):
-    pass
+def get_id():
+	return "000"
+
+def check():
+	try:
+		with get_transport('ssh') as ssh_transport:
+			i, o, e = ssh_transport.exec("[ -f /testfile ] && echo '1' || echo '2'")
+			return o.read()[0] - 48
+	except TransportConnetionError as e:
+		print(str(e))
+		return 4
+	except Exception as e:
+		print(str(e))
+		return 5
