aaeb71a059851ace543acc71163d906d7a5f4466
Your Name <you@example.com>
2018-05-06 21:48:31 +0700

    usage example

--- src/main.py
+++ src/main.py
@@ -2,10 +2,14 @@
 
 import os
 import importlib
+from reportdb import add_control
+from reportdb import get_scandata
 
 if __name__ == '__main__':
 	files = [f.split('.')[0] for f in os.listdir('./scripts') if os.path.isfile(os.path.join("./scripts", f))]
 	for file in files:
 		module = importlib.import_module("scripts.{}".format(file))
-		print(module.check())
+		add_control(module.get_id(), module.check())
 
+	for row in get_scandata():
+		print(row)
